8325646511c8b9d73797b0b7c525b46811a447f0
Sedrick Moore II <sedrickmoore@2gmail.com>
2024-07-05 20:49:10 -0700

    changed style of player cards. made them more rounded, centered description, removed bullet points, made header and list closer.

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,49 +1,55 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Grid from "@mui/material/Grid";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
 export default function PlayerCard(player) {
   return (
     <Grid item xs={12} md={4}>
-            <Card sx={{ 
-              backgroundColor: 'rgba(255, 255, 255, 0.5)', 
-              backdropFilter: 'blur(8px)',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-              borderRadius: 8,
-              p: 2,
-              color:"black", 
-              width:350, 
-              justifyContent:"center"}}>
-              <CardHeader
-              title={player.FirstName +
-                " " +
-                player.LastName}
-                titleTypographyProps={{ align: "center" }}
-                sx={{ mt: 1}}
-              />
-              <CardContent sx={{ pt: 10}}>
-                <ul>
-                   <Typography component="li">
-                      {"Team: " + player.Team}
-                    </Typography>
-                    <Typography component="li">
-                      {"Position: " + player.Position}
-                    </Typography>
-                    <Typography component="li">
-                      {"Height (in.): " + player.Height}
-                    </Typography>
-                    <Typography component="li">
-                      {"Weight (lbs.): " + player.Weight}
-                    </Typography>
-                    <Typography component="li">
-                      {"Birth Year: " + player.BirthDate?.substring(0, 4)}
-                    </Typography>
-                </ul>
-              </CardContent>
-            </Card>
-          </Grid>
+      <Card
+        sx={{
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          backdropFilter: "blur(8px)",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          borderRadius: 100,
+          p: 2,
+          color: "black",
+          width: 350,
+          justifyContent: "center",
+        }}
+      >
+        <CardHeader
+          title={player.FirstName + " " + player.LastName}
+          titleTypographyProps={{ align: "center" }}
+          // sx={{ mt: 1 }}
+        />
+        <CardContent >
+          <ul
+            style={{ listStyleType: "none", padding: 0, textAlign: "center" }}
+          >
+            <li>
+              <Typography>{"Team: " + player.Team}</Typography>
+            </li>
+            <li>
+              <Typography>{"Position: " + player.Position}</Typography>
+            </li>
+            <li>
+              <Typography>{"Height (in.): " + player.Height}</Typography>
+            </li>
+            <li>
+              <Typography>{"Weight (lbs.): " + player.Weight}</Typography>
+            </li>
+            <li>
+              <Typography>
+                {"Birth Year: " +
+                  (player.BirthDate ? player.BirthDate.substring(0, 4) : "")}
+              </Typography>
+            </li>
+          </ul>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
